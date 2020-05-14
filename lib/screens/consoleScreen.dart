5bdc0145efdb95fec8e0a3ff2370f732a76f31e7
alkimo <alkimo.protonmail@gmail.com>
2020-05-14 20:03:29 -0300

    Add a scrollable Row with SingleChildScrollView on the consoleScreen

--- lib/screens/consoleScreen.dart
+++ lib/screens/consoleScreen.dart
@@ -10,10 +10,92 @@ class ConsoleScreen extends StatefulWidget {
 
 class _ConsoleScreenState extends State<ConsoleScreen> {
   @override
+  void initState() {
+    print(widget.consoleIndexValue);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Text(widget.consoleIndexValue),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            color: Colors.black12,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    maxRadius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.icon-icons.com/icons2/1506/PNG/512/emblemok_103757.png'),
+                  ),
+                  CircleAvatar(
+                    maxRadius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.icon-icons.com/icons2/1506/PNG/512/emblemok_103757.png'),
+                  ),
+                  CircleAvatar(
+                    maxRadius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.icon-icons.com/icons2/1506/PNG/512/emblemok_103757.png'),
+                  ),
+                  CircleAvatar(
+                    maxRadius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.icon-icons.com/icons2/1506/PNG/512/emblemok_103757.png'),
+                  ),
+                  CircleAvatar(
+                    maxRadius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.icon-icons.com/icons2/1506/PNG/512/emblemok_103757.png'),
+                  ),
+                  CircleAvatar(
+                    maxRadius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.icon-icons.com/icons2/1506/PNG/512/emblemok_103757.png'),
+                  ),
+                  CircleAvatar(
+                    maxRadius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.icon-icons.com/icons2/1506/PNG/512/emblemok_103757.png'),
+                  ),
+                  CircleAvatar(
+                    maxRadius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://cdn.icon-icons.com/icons2/1506/PNG/512/emblemok_103757.png'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.white,
+            child: Center(
+              child: Text("OK"),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.white,
+            child: Center(
+              child: Text("OK"),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.white,
+            child: Center(
+              child: Text("OK"),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
